c2d078314fde6b456d65b91f3a7a913be7795850
SpiDher <penivera655@gmail.com>
2025-01-22 01:46:24 +0100

    Connection works

--- main.py
+++ main.py
@@ -47,3 +47,5 @@ async def root(request: UserCreate, db: AsyncSession = Depends(get_db)) -> UserS
     # users= result.scalars().all()
     # return users
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
